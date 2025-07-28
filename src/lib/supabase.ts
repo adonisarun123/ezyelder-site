9393262db9c92701ce36d99ebed9dbddc1f66be1
astikezyhelpers <astik.ezyhelpers@gmail.com>
2025-07-28 17:23:14 +0530

    Changed Supabase Creds

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,16 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
-  throw new Error('Missing environment variable NEXT_PUBLIC_SUPABASE_URL')
+if (!process.env.REACT_APP_SUPABASE_URL) {
+  throw new Error('Missing environment variable REACT_APP_SUPABASE_URL')
 }
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error('Missing environment variable NEXT_PUBLIC_SUPABASE_ANON_KEY')
+if (!process.env.REACT_APP_SUPABASE_ANON_KEY) {
+  throw new Error('Missing environment variable REACT_APP_SUPABASE_ANON_KEY')
 }
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
 )
 
 // Types
